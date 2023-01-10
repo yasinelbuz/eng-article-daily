b0a8e3b5cacdfafe4fbd318e9f89f77c70a8b15e
Yasin Elbüz <yasinelbuz@gmail.com>
2023-01-11 02:45:19 +0300

    fix:redux

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "@/styles/index.scss";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "@/stores";
+import { store } from "@/stores";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<Provider store={store}>
